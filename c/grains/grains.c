a3d7f5b0aef5297b8e899f1c4de97179c5b490a4
d0bhareach <viacheslav.modestov@gmail.com>
2024-03-26 21:54:40 +0100

    c: grains done

--- c/grains/grains.c
+++ c/grains/grains.c
@@ -1,1 +1,18 @@
 #include "grains.h"
+#include <math.h>
+
+uint64_t square(uint8_t index)
+{
+        if (index == 0 || index ==1) return index;
+        else if (index > 64) return 0;
+        return 1 * powl(2, (index - 1));
+}
+
+uint64_t total(void)
+{
+        uint64_t res = 0;
+        for (int i = 1; i < 65; i++){
+                res += square(i);
+        }
+        return res;
+}
