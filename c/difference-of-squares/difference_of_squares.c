f87ccd6cf4186c7d0c6db70a0c631e64744bd8ee
d0bhareach <viacheslav.modestov@gmail.com>
2024-03-27 19:44:02 +0100

    c: difference-of-squares done

--- c/difference-of-squares/difference_of_squares.c
+++ c/difference-of-squares/difference_of_squares.c
@@ -1,1 +1,19 @@
 #include "difference_of_squares.h"
+
+// (n(n+1)(2n+1))/6 Johann Faulhaber's formula
+unsigned int sum_of_squares(unsigned int number)
+{
+        return (number * (number + 1) * ((2* number) + 1)) / 6;
+}
+
+// (n*(n+1))/2 specific case of the Faulhaber's formula
+unsigned int square_of_sum(unsigned int number)
+{
+        unsigned int n = (number * (number + 1)) / 2;
+        return n * n;
+}
+
+unsigned int difference_of_squares(unsigned int number)
+{
+        return square_of_sum(number) - sum_of_squares(number);
+}
