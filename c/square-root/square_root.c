c4c1457c97fed7f4d3e5e65966af3cc12132bff3
d0bhareach <viacheslav.modestov@gmail.com>
2024-03-27 23:00:53 +0100

    c: square-root done

--- c/square-root/square_root.c
+++ c/square-root/square_root.c
@@ -1,1 +1,16 @@
 #include "square_root.h"
+#include <math.h>
+
+unsigned int square_root(unsigned int num)
+{
+        if (num == 1) return num;
+        float prev = 1.0;
+        float r = 1.0;
+        float epsylon = 0.1;
+        while (1 == 1) {
+                prev = r;
+                r = 0.5 * (r + (num / r));
+                if (fabs(prev -r) < epsylon) return r;
+        }
+        return r;
+}
