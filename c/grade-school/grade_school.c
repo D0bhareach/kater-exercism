aa60561a278847f5cd9fe7ae9838ec0dfbb5026a
d0bhareach <viacheslav.modestov@gmail.com>
2024-03-30 17:58:48 +0100

    c:grade-school done

--- c/grade-school/grade_school.c
+++ c/grade-school/grade_school.c
@@ -1,22 +1,7 @@
 #include "grade_school.h"
 #include <stdlib.h>
 #include <string.h>
-//
-#include <stdio.h>
-/*
- *
- typedef struct {
- size_t count;
- student_t students[MAX_STUDENTS];
- } roster_t;
 
-
- typedef struct {
- uint8_t grade;
- char name[MAX_NAME_LENGTH];
- } student_t;
- * */
-// My functions
 int compare_grade(const void *a, const void *b);
 int compare_name(const void *a, const void *b);
 int roster_has_student(roster_t *roster, char * name);
@@ -26,7 +11,6 @@ int compare_grade(const void *a, const void *b)
         uint8_t grade1 = ((const student_t *)a)->grade;
         uint8_t grade2 = ((const  student_t *)b)->grade;
         if (grade2 == 255) return -1;
-        // printf("1grade: %d, 2grade: %d\n", grade1, grade2);
         return (grade1 - grade2);
 }
 
@@ -34,27 +18,16 @@ int compare_name(const void *a, const void *b)
 {
         const char *n1 = ((const student_t *)a)->name;
         const char *n2 = ((const student_t *)b)->name;
-        // const uint8_t g = ((const student_t *)b)->grade;
-        // if (*n2 == '\0' || g == 255) return -1;
-        // printf("%.*s",len,buf);
-        // printf("compare_name- 1name: %.*s, 2name: %.*s\n", 20, n1, 20, n2);
         return strcmp(n1, n2);
 }
 
-/*
- *If roster has student will return int > 0;
- * */
 int roster_has_student(roster_t *roster, char * name)
 {
-        // if roster students have the same name they can not be in the roster
-        // student is unique by name.
         int res = 0;
         for (int i = 0; i  < MAX_STUDENTS; i++){
                 student_t s = roster->students[i];
                 int cmp = strcmp(s.name, name);
                 if (cmp == 0){
-                        // printf("grade is different, but names are the same.\n");
-                        // printf("s.name: %s, name: %s\n", s.name, name);
                         return 1;
                 }
 
@@ -65,13 +38,12 @@ int roster_has_student(roster_t *roster, char * name)
 void init_roster(roster_t *rost)
 {
         rost->count = 0;
-        // create dummy student
+
         student_t s;
         s.grade = 255;
         for (int i = 0; i <  MAX_NAME_LENGTH; i++){
                 s.name[i] = '\0';
         }
-        // add dummy student to students
         for (int i = 0; i  < MAX_STUDENTS; i++){
                 rost->students[i] = s;
         }
@@ -101,7 +73,6 @@ roster_t get_grade(roster_t *rost, uint8_t desired)
 {
         roster_t r;
         r.count = 0;
-        // loop and add if grade is eq
         for (int i = 0; i  < MAX_STUDENTS; i++){
                 student_t s = rost->students[i];
                 if (s.grade == desired) {
@@ -110,14 +81,5 @@ roster_t get_grade(roster_t *rost, uint8_t desired)
                 }
 
         }
-
-        // printf("{\n");
-        // printf("count: %ld\n", r.count);
-        // for (int i = 0; i  < MAX_STUDENTS; i++){
-        //         student_t s = r.students[i];
-        //         if (s.grade !=255 && s.name[0] != '\0')
-        //                 printf("name: %s, grade: %d\n", s.name, s.grade);
-        // }
-        // printf("}\n");
         return r;
 }
--- c/grade-school/test_grade_school.c
+++ c/grade-school/test_grade_school.c
@@ -40,7 +40,7 @@ static void check_rosters(roster_t expected, roster_t actual)
 
 static void test_roster_is_empty_when_no_student_added(void)
 {
-        TEST_IGNORE();   // delete this line to run test
+        // TEST_IGNORE();   // delete this line to run test
         roster_t actual = NON_EMPTY_DUMMY_ROSTER;
         init_roster(&actual);
         TEST_ASSERT_EQUAL(0, actual.count);
@@ -48,7 +48,7 @@ static void test_roster_is_empty_when_no_student_added(void)
 
 static void test_add_student(void)
 {
-        TEST_IGNORE();   // delete this line to run test
+        // TEST_IGNORE();   // delete this line to run test
         roster_t actual;
         init_roster(&actual);
         TEST_ASSERT_TRUE(add_student(&actual, "Aimee", 2));
@@ -56,7 +56,7 @@ static void test_add_student(void)
 
 static void test_student_added_to_roster(void)
 {
-        TEST_IGNORE();
+        // TEST_IGNORE();
         roster_t expected = { 1, { (student_t){ 2, "Aimee" } } };
         roster_t actual;
         init_roster(&actual);
@@ -68,7 +68,7 @@ static void test_student_added_to_roster(void)
 
 static void test_adding_multiple_students_in_same_grade_in_roster(void)
 {
-        TEST_IGNORE();
+        // TEST_IGNORE();
         roster_t actual;
         init_roster(&actual);
 
@@ -79,7 +79,7 @@ static void test_adding_multiple_students_in_same_grade_in_roster(void)
 
 static void test_multiple_students_in_same_grade_are_added_to_roster(void)
 {
-        TEST_IGNORE();
+        // TEST_IGNORE();
         roster_t expected = { 3,
                 { (student_t){ 2, "Blair" }, (student_t){ 2, "James" },
                         (student_t){ 2, "Paul" } } };
@@ -96,7 +96,7 @@ static void test_multiple_students_in_same_grade_are_added_to_roster(void)
 
 static void test_cannot_add_student_to_same_grade_more_than_once(void)
 {
-        TEST_IGNORE();
+        // TEST_IGNORE();
         roster_t actual;
         init_roster(&actual);
 
@@ -108,7 +108,7 @@ static void test_cannot_add_student_to_same_grade_more_than_once(void)
 
 static void test_student_not_added_to_same_grade_in_roster_more_than_once(void)
 {
-        TEST_IGNORE();
+        // TEST_IGNORE();
         roster_t expected = { 3,
                 { (student_t){ 2, "Blair" }, (student_t){ 2, "James" },
                         (student_t){ 2, "Paul" } } };
@@ -125,7 +125,7 @@ static void test_student_not_added_to_same_grade_in_roster_more_than_once(void)
 
 static void test_adding_students_in_multiple_grades(void)
 {
-        TEST_IGNORE();
+        // TEST_IGNORE();
         roster_t actual;
         init_roster(&actual);
 
@@ -135,7 +135,7 @@ static void test_adding_students_in_multiple_grades(void)
 
 static void test_students_in_multiple_grades_are_added_to_roster(void)
 {
-        TEST_IGNORE();
+        // TEST_IGNORE();
         roster_t expected = {
                 2,
                 { (student_t){ 3, "Chelsea" }, (student_t){ 7, "Logan" } },
@@ -151,7 +151,7 @@ static void test_students_in_multiple_grades_are_added_to_roster(void)
 
 static void test_cannot_add_same_student_to_multiple_grades_in_roster(void)
 {
-        TEST_IGNORE();
+        // TEST_IGNORE();
         roster_t actual;
         init_roster(&actual);
 
@@ -163,7 +163,7 @@ static void test_cannot_add_same_student_to_multiple_grades_in_roster(void)
 
 static void test_student_not_added_to_multiple_grades_in_roster(void)
 {
-        TEST_IGNORE();
+        // TEST_IGNORE();
         roster_t expected = { 3,
                 { (student_t){ 2, "Blair" }, (student_t){ 2, "James" },
                         (student_t){ 3, "Paul" } } };
@@ -180,7 +180,7 @@ static void test_student_not_added_to_multiple_grades_in_roster(void)
 
 static void test_students_are_sorted_by_grades_in_roster(void)
 {
-        TEST_IGNORE();
+        // TEST_IGNORE();
         roster_t expected = { 3,
                 { (student_t){ 1, "Anna" }, (student_t){ 2, "Peter" },
                         (student_t){ 3, "Jim" } } };
@@ -196,7 +196,7 @@ static void test_students_are_sorted_by_grades_in_roster(void)
 
 static void test_students_are_sorted_by_name_in_roster(void)
 {
-        TEST_IGNORE();
+        // TEST_IGNORE();
         roster_t expected = { 3,
                 { (student_t){ 2, "Alex" }, (student_t){ 2, "Peter" },
                         (student_t){ 2, "Zoe" } } };
@@ -212,7 +212,7 @@ static void test_students_are_sorted_by_name_in_roster(void)
 
 static void test_students_are_sorted_by_grades_and_then_by_names_in_roster(void)
 {
-        TEST_IGNORE();
+        // TEST_IGNORE();
         roster_t expected = { 7,
                 { (student_t){ 1, "Anna" }, (student_t){ 1, "Barb" },
                         (student_t){ 1, "Charlie" },
@@ -234,7 +234,7 @@ static void test_students_are_sorted_by_grades_and_then_by_names_in_roster(void)
 
 static void test_grade_empty_if_no_students_in_roster(void)
 {
-        TEST_IGNORE();
+        // TEST_IGNORE();
         uint8_t desired_grade = 1;
         roster_t roster;
         init_roster(&roster);
@@ -246,7 +246,7 @@ static void test_grade_empty_if_no_students_in_roster(void)
 
 static void test_grade_empty_if_no_students_in_grade(void)
 {
-        TEST_IGNORE();
+        // TEST_IGNORE();
         uint8_t desired_grade = 1;
         roster_t roster;
         init_roster(&roster);
@@ -263,7 +263,7 @@ static void test_grade_empty_if_no_students_in_grade(void)
 
 static void test_student_not_added_to_same_grade_more_than_once(void)
 {
-        TEST_IGNORE();
+        // TEST_IGNORE();
         uint8_t desired_grade = 2;
         roster_t expected = { 3,
                 { (student_t){ 2, "Blair" }, (student_t){ 2, "James" },
@@ -283,7 +283,7 @@ static void test_student_not_added_to_same_grade_more_than_once(void)
 
 static void test_student_not_added_to_multiple_grades(void)
 {
-        TEST_IGNORE();
+        // TEST_IGNORE();
         uint8_t desired_grade = 2;
         roster_t expected = {
                 2, { (student_t){ 2, "Blair" }, (student_t){ 2, "James" } }
@@ -303,7 +303,7 @@ static void test_student_not_added_to_multiple_grades(void)
 
 static void test_student_not_added_to_other_grade_for_multiple_grades(void)
 {
-        TEST_IGNORE();
+        // TEST_IGNORE();
         uint8_t desired_grade = 3;
         roster_t expected = { 1, { (student_t){ 3, "Paul" } } };
         roster_t roster;
