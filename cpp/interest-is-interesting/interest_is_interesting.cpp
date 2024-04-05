bfa39cdc5c6d296fa2c6d8420f264d712ca7d661
d0bhareach <viacheslav.modestov@gmail.com>
2024-04-05 21:02:33 +0200

    cpp: interest-is-interesting done

---
+++ cpp/interest-is-interesting/interest_is_interesting.cpp
@@ -0,0 +1,35 @@
+double interest_rate(double balance) {
+    if (balance < 0) {
+        return 3.213;
+    } else if (balance >= 0 && balance < 1000) {
+        return 0.5;
+    } else if (balance >= 1000 && balance < 5000) {
+        return 1.621;
+    } else if (balance >= 5000) {
+        return 2.475;
+    }
+    return 0.0;
+}
+
+// yearly_interest calculates the yearly interest for the provided balance.
+double yearly_interest(double balance) {
+    return (balance * interest_rate(balance)) / 100;
+}
+
+// annual_balance_update calculates the annual balance update, taking into
+// account the interest rate.
+double annual_balance_update(double balance) {
+    return balance + yearly_interest(balance);
+}
+
+// years_until_desired_balance calculates the minimum number of years required
+// to reach the desired balance.
+int years_until_desired_balance(double balance, double target_balance) {
+    double b = balance;
+    int year = 0;
+    while (b < target_balance) {
+        b = annual_balance_update(b);
+        year++;
+    }
+    return year;
+}
