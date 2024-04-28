f9c0d08d4e941a6a142e19179bd638077bb08f13
d0bhareach <viacheslav.modestov@gmail.com>
2024-04-28 15:10:50 +0200

    cpp: raindrops with map and iter

--- cpp/raindrops/raindrops.cpp
+++ cpp/raindrops/raindrops.cpp
@@ -1,31 +1,30 @@
 #include <iostream>
 #include <sstream>
+#include <map>
 #include "raindrops.h"
 
 namespace raindrops {
+    std::map<int, std::string> drops = {
+            {3, "Pling"},
+            {5, "Plang"},
+            {7, "Plong"},
+    };
 
     std::string convert(int i)
     {
         std::stringstream ss;
         bool have = false;
-            if ((i % 3) == 0){
-                 ss << "Pling";
+        for (auto const& pair:drops) {
+            if ((i % pair.first) == 0){
+                 ss << pair.second;
                  have = true;
             }
+                
+        }
 
-            if ((i % 5) == 0) {
-                 ss << "Plang";
-                 have = true;
-            }
-
-            if ((i % 7) == 0) {
-                 ss << "Plong";
-                 have = true;
-            }
-
-            if (!(have)) {
-                ss << i;
-            }
+        if (!(have)) {
+            ss << i;
+        }
         return ss.str();
     }
 }
